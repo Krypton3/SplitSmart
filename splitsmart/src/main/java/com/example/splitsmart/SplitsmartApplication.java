b61a69419a3287a2d01e7d051f361e9620de84e6
H M Mahedi Hasan <jisan2723@gmail.com>
2024-12-15 12:55:24 -0700

    Add backend structure for billbuddies

---
+++ splitsmart/src/main/java/com/example/splitsmart/SplitsmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.splitsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitsmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitsmartApplication.class, args);
+	}
+
+}
---
+++ splitsmart/src/test/java/com/example/splitsmart/SplitsmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splitsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitsmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
